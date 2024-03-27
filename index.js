e5e2bd491e7fc6a59bdce64754c9c1b68b08a174
Aman Mishra <mishraaman2021@gmail.com>
2024-03-27 13:03:52 +0530

    use ejs-mate

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const {indiaData} = require('./citydata')
+const ejsMate = require('ejs-mate')
 const app = express()
 const port  = 3000;
 
+app.engine('ejs', ejsMate)
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 // app.use('views', path.join(__dirname,'views'))
@@ -18,7 +20,7 @@ app.get('/Adopt',(req,res)=>{
     res.render('dogs',{state,cities})
 })
 
-app.get('/aboutDog',(req,res)=>{
+app.get('/aboutDog/1',(req,res)=>{
     res.render("Dogo-adopt")
 })
 
